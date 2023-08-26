3c37b441ebea8fd3fc89e64076a77fe879f363be
mh@imac <mjdhasan@icloud.com>
2023-08-26 11:27:55 +0800

    added org_id in push_model_to_hf_hub()

--- src/push_to_hf_hub.py
+++ src/push_to_hf_hub.py
@@ -9,7 +9,8 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 
 def push_model_to_hf_hub(
         model_id: str,
-        checkpoints_dir: str,
+        checkpoints_dir: str = '/code/data/checkpoints',
+        org_id: str = 'ESGenie',
         hf_token: str = "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",
 ):
     ## login to huggingface
@@ -29,13 +30,60 @@ def push_model_to_hf_hub(
     ## read fine-tuned model
     model = PeftModel.from_pretrained(
         model=model,
-        model_id=model_id,
+        model_id=f"/code/data/checkpoints",
         use_auth_token=True,
         is_trainable=True,
     )
     ## push model to hub
-    model.push_to_hub(model_id)
+    model.push_to_hub(f"{org_id}/{model_id}")
     ## push tokenizer
-    tokenizer.push_to_hub(model_id)
+    tokenizer.push_to_hub(f"{org_id}/{model_id}")
     ## return model_id
     return {'status': 'successful', 'data': model_id}
+
+
+if __name__ == "__main__":
+    import argparse
+    ## Create the parser
+    arg_parser = argparse.ArgumentParser(
+        prog='llm-genie',
+        usage='%(prog)s [options]',
+        description='LLM-Genie'
+    )
+    ## add arg: model_id
+    arg_parser.add_argument('-mid', '--model_id',
+                            metavar='model_id',
+                            nargs='?',
+                            type=str,
+                            help='model id')
+    ## add arg: checkpoints_dir
+    arg_parser.add_argument('-cdir', '--checkpoints_dir',
+                            metavar='checkpoints_dir',
+                            nargs='?',
+                            type=str,
+                            default='/code/data/checkpoints',
+                            help='local checkpoints directory')
+    ## add arg: HF org id
+    arg_parser.add_argument('-hf_org', '--hf_org',
+                            metavar='hforg',
+                            nargs='?',
+                            type=str,
+                            default='ESGenie',
+                            help='HuggingFace organisation id')
+    ## add arg: HF token (with write permissions)
+    arg_parser.add_argument('-hf_token', '--hf_token',
+                            metavar='hf_token',
+                            nargs='?',
+                            type=str,
+                            default='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',
+                            help='HuggingFace token with write permissions')
+    ## parse args
+    args, unknown_args = arg_parser.parse_known_args()
+    ## push model to hub
+    msg = push_model_to_hf_hub(
+        model_id=args.model_id,
+        checkpoints_dir=args.checkpoints_dir,
+        org_id=args.hf_org,
+        hf_token=args.hf_token,
+    )
+    print(f"msg: {msg}")
